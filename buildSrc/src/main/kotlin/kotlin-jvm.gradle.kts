7b9e30a4a1682e0fe4d8528b6ab51420bebcc132
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2021-06-30 15:49:03 +0200

    Improve sourceSets

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -8,5 +8,10 @@ version = "1.0.0"
 
 kotlin.sourceSets.all {
     kotlin.srcDirs(name)
-    resources.srcDirs(name)
+    resources.srcDirs("resources")
+}
+
+java.sourceSets.all {
+    java.srcDirs(name)
+    resources.srcDirs("resources")
 }
